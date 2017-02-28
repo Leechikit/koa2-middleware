454c5c444b67ee6918da8d17ca82459c20b6a159
李子杰 <leechikit0823@163.com>
2017-02-28 11:05:14 +0800

    update

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const logger = require('koa-logger');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
+const api = require('./routes/api');
+const response_formatter = require('./middlewares/response_formatter');
 
 // middlewares
 app.use(convert(bodyparser));
@@ -22,13 +24,35 @@ app.use(views(__dirname + '/views', {
   extension: 'jade'
 }));
 
+//log工具
+const logUtil = require('./utils/log_util');
+
 // logger
 app.use(async (ctx, next) => {
+  //响应开始时间
   const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  //响应间隔时间
+  var ms;
+  try {
+    //开始进入到下一个中间件
+    await next();
+
+    ms = new Date() - start;
+    //记录响应日志
+    logUtil.logResponse(ctx, ms);
+
+  } catch (error) {
+
+    ms = new Date() - start;
+    //记录异常日志
+    logUtil.logError(ctx, error, ms);
+  }
 });
+
+//仅对/api开头的url进行格式化处理
+app.use(response_formatter('^/api'));
+
+router.use('/api', api.routes(), api.allowedMethods());
 
 router.use('/', index.routes(), index.allowedMethods());
 router.use('/users', users.routes(), users.allowedMethods());
